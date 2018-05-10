ee8b7a45e74433033fb2c8417c4187c0580b9881
hawk <briochieng97@gmail.com>
2018-05-11 02:24:40 +0300

    Update locker.php

--- src/locker.php
+++ src/locker.php
@@ -1,6 +1,6 @@
 <?php 
                         #===================CACHE BLOCKER============================#
-    /* You may have a config file where you set whether an update is up or not to make the users load the files only just after the update
+    /* You may have a config file where you'll set whether an update is up or not, thi is to ensure the users load the files only just after the update
     since browsers clear their cache after a while */
   $config['new_version'] = true; // setting whether there is a new version.
  
