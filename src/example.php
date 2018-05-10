4dbb1b25235db2c81773e3b9740fb654d4508597
hawk <briochieng97@gmail.com>
2018-05-11 02:19:01 +0300

    Create example.php

---
+++ src/example.php
@@ -0,0 +1,20 @@
+<?php 
+                        #===================CACHE BLOCKER============================#
+    /* You may have a config file where you set whether an update is up or not to make the users load the files only just after the update
+    since browsers clear their cache after a while */
+  $config['new_version'] = true; // setting whether there is a new version.
+ 
+  if($config['new_version']){
+    $time = time();
+  }else{
+    $time = "";
+  }
+  
+  /*after setting the time value you pass it as a $_GET field when sending the $_GET request for the file.*/
+  
+                        #-----------------------------------------------------------#
+  $uri = "/js/js_file.s?".$time;
+  
+  echo  '<script type="text/javascript" src="'.$uri.'"></script>';
+  
+?>
